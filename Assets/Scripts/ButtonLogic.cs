0509d61a767544439aba750c64a06ede43392059
Chen Su <martinsuchen3557@gmail.com>
2017-08-13 12:30:19 -0400

    Change button color when pointer enter/exit

---
+++ Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonLogic : MonoBehaviour {
+
+    public Button button;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClicked()
+    {
+        Debug.Log("Clicked");
+    }
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("OnPointerEnter");
+        ColorBlock cb = button.colors;
+        cb.normalColor = new Color(3F, 169F, 244F, 255F);
+        button.colors = cb;
+    }
+
+    public void OnPointerExit()
+    {
+        Debug.Log("OnPointerExit");
+        ColorBlock cb = button.colors;
+        cb.normalColor = new Color(255F, 255F, 255F, 255F);
+        button.colors = cb;
+    }
+}
--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -21,4 +21,5 @@ public class GameLogic : MonoBehaviour {
 		startUI.SetActive (!startUI.activeSelf);
 		restartUI.SetActive (!restartUI.activeSelf);
 	}
+
 }
