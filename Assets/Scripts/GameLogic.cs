14848add72f46251fa28fd46689e49bfbfe4d65f
Chen Su <martinsuchen3557@gmail.com>
2017-08-13 18:59:53 -0400

    Use itween to get moving

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -7,19 +7,59 @@ public class GameLogic : MonoBehaviour {
 	public GameObject startUI, restartUI;
 
 
-	// Use this for initialization
-	void Start () {
+    public GameObject startPoint, playPoint, restartPoint;
 
-	}
+    public bool playerWon = false;
 
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        player.transform.position = startPoint.transform.position;
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && player.transform.position == playPoint.transform.position)
+        {
+            puzzleSuccess();
+        }
+    }
 
-	public void toggleUI() {
-		startUI.SetActive (!startUI.activeSelf);
-		restartUI.SetActive (!restartUI.activeSelf);
-	}
+    public void startPuzzle()
+    { //Begin the puzzle sequence
+        toggleUI();
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", playPoint.transform.position,
+                "time", 2,
+                "easetype", "linear"
+            )
+        );
+
+    }
+
+    public void resetPuzzle()
+    { //Reset the puzzle sequence
+        player.transform.position = startPoint.transform.position;
+        toggleUI();
+    }
+
+
+    public void puzzleSuccess()
+    { //Do this when the player gets it right
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", restartPoint.transform.position,
+                "time", 2,
+                "easetype", "linear"
+            )
+        );
+    }
+    public void toggleUI()
+    {
+        startUI.SetActive(!startUI.activeSelf);
+        restartUI.SetActive(!restartUI.activeSelf);
+    }
 
 }
