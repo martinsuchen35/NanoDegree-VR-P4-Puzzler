6d0bc6570b707d41b1f6a76eb15037bec78e7754
Chen Su <martinsuchen3557@gmail.com>
2017-08-13 20:34:11 -0400

    Wire up game logic

--- Assets/Scripts/lightUp.cs
+++ Assets/Scripts/lightUp.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class lightUp : MonoBehaviour {
-	/*public Material lightUpMaterial;
+	public Material lightUpMaterial;
 	public GameObject gameLogic;
 	private Material defaultMaterial;
 
@@ -33,8 +33,10 @@ public class lightUp : MonoBehaviour {
 
 	}
 	public void playerSelection() {
-		//GameLogic.GetComponent<gameLogic>().playerSelection(this.gameObject);
-		//this.GetComponent<GvrAudioSource>().Play();
+        Debug.Log("lightUp.playerSelection()");
+
+		//this.GetComponent<GameLogic>().playerSelection(this.gameObject);
+		this.GetComponent<GvrAudioSource>().Play();
 	}
 	public void aestheticReset() {
 		this.GetComponent<MeshRenderer>().material = defaultMaterial; //Revert to the default material
@@ -52,5 +54,5 @@ public class lightUp : MonoBehaviour {
 		patternLightUp ();
 		yield return new WaitForSeconds(duration-.1f);
 		aestheticReset ();
-	}*/
+	}
 }
